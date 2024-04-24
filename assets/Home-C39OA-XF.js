f7c23800642abfd666d26b809c19559966c9433f
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-24 19:34:32 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@609f5a44887dd1086df8a279b0e84dc23f1f0f7b 🚀

---
+++ assets/Home-C39OA-XF.js
@@ -0,0 +1,3 @@
+import{u as t,j as e}from"./index-Dy_FBO2F.js";const s=t.p`
+  color: ${o=>o.theme.colors.textColor};
+`,a=()=>e.jsx("div",{className:"container",children:e.jsx(s,{children:"It`s Home Page"})});export{a as default};
