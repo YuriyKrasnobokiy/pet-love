5bac29810d31f05d381d56c3a9028c53a000bd25
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-27 21:06:51 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@166380f4d0acb6dda82478ee56b708cbc69e43be 🚀

--- assets/Home-CZlI_ZeW.js
+++ assets/Home-CZlI_ZeW.js
@@ -1,3 +1,3 @@
-import{u as t,j as e}from"./index-iYN48D-c.js";const s=t.p`
+import{u as t,j as e}from"./index-BwqU3oRH.js";const s=t.p`
   color: ${o=>o.theme.colors.textColor};
 `,a=()=>e.jsx("div",{className:"container",children:e.jsx(s,{children:"It`s Home Page"})});export{a as default};
