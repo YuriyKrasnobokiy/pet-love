ba7cc3190cad629f17de337b1d54bb68d5abd15f
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-27 22:12:17 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@903ad47e28e2489bd470fa767b0fca68a09b2621 🚀

--- assets/News-DVAVALKY.js
+++ assets/News-DVAVALKY.js
@@ -1,3 +1,3 @@
-import{u as t,j as e}from"./index-BwqU3oRH.js";const o=t.p`
+import{u as t,j as e}from"./index-DDhux8jS.js";const o=t.p`
   color: ${s=>s.theme.colors.textColor};
 `,a=()=>e.jsx("div",{className:"container",children:e.jsx(o,{children:"It`s News Page"})});export{a as default};
