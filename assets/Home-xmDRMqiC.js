577d3e06dc48a21b250341c7ce2f9ddc7487c401
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-04-29 19:25:15 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@66cdf2d2b3b50f0b525fcd371a36b6b9b15420b2 🚀

---
+++ assets/Home-xmDRMqiC.js
@@ -0,0 +1,25 @@
+import{u as t,j as o}from"./index-CsfAJq9K.js";const s=t.div`
+  padding: 0 20px 10px;
+`,r=t.div`
+  padding: 50px 20px;
+  border-radius: 30px;
+  text-align: start;
+  background-color: ${e=>e.theme.colors.HomeBgColor};
+`,n=t.h1`
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 0.96;
+  letter-spacing: -0.03em;
+  color: ${e=>e.theme.colors.HomeTextColor};
+  margin-bottom: 24px;
+`,i=t.span`
+  color: ${e=>e.theme.colors.HomeTextAccentColor};
+`,c=t.p`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1.28571;
+  letter-spacing: -0.02em;
+  color: ${e=>e.theme.colors.HomeTextColor};
+`,m=t.img`
+  border-radius: 30px;
+`,a=()=>o.jsxs(s,{className:"container",children:[o.jsxs(r,{children:[o.jsxs(n,{children:["Take good ",o.jsx(i,{children:"care"})," of your small pets"]}),o.jsx(c,{children:"Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable joy and tenderness."})]}),o.jsx("div",{children:o.jsx(m,{src:"src/assets/imgs/mobile/home-img-1x-mob.jpg",alt:"pet"})})]});export{a as default};
