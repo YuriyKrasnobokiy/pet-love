bce46a69cfc09d3d94149aac704d3b145e6a15c4
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-05-06 07:30:49 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@114ca1eef28787cc2ecdf81627bda343f2c24662 🚀

--- assets/Home-QTxjhdBw.js
+++ assets/Home-QTxjhdBw.js
@@ -1,4 +1,4 @@
-import{u as t,j as o}from"./index-CdlN-fPI.js";const s=t.div`
+import{u as t,j as o}from"./index-yHwws0pe.js";const s=t.div`
   padding: 0 20px 10px;
 `,r=t.div`
   padding: 50px 20px;
--- assets/News-caG_-XD5.js
+++
@@ -1,3 +0,0 @@
-import{u as t,j as e}from"./index-CdlN-fPI.js";const o=t.p`
-  color: ${s=>s.theme.colors.textColor};
-`,a=()=>e.jsx("div",{className:"container",children:e.jsx(o,{children:"It`s News Page"})});export{a as default};
