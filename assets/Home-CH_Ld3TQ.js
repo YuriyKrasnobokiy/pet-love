2b6aadaffaf829af9b97984ca1accf553835ae41
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-05-26 17:36:02 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@214dadc875973c47973168fcc277f09b0f69e673 🚀

--- assets/Home-CH_Ld3TQ.js
+++ assets/Home-CH_Ld3TQ.js
@@ -1,4 +1,4 @@
-import{u as t,j as o}from"./index-YDpxylkl.js";const s=t.div`
+import{u as t,j as o}from"./index-CgbaZ9fL.js";const s=t.div`
   padding: 0 20px 10px;
 `,r=t.div`
   padding: 50px 20px;
