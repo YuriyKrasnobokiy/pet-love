e8e8f0fff6fc6152ad02e24fb09d09b0e8c475e0
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-07-01 09:00:53 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@53cc88b25bec1f9db592bfb2cd255dc82757cd37 🚀

--- assets/Home-DsZQFqQQ.js
+++ assets/Home-DsZQFqQQ.js
@@ -1,4 +1,4 @@
-import{u as t,j as o}from"./index-Bti_3NRF.js";const s=t.div`
+import{u as t,j as o}from"./index-qI-2zD93.js";const s=t.div`
   padding: 0 20px 10px;
 `,r=t.div`
   padding: 50px 20px;
