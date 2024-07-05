eecf6fdcc82a3631e9aa24fc92a61f26b10a1e5b
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-07-05 08:46:25 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@7c04fc3cb085939e854868c36342d6f3f0d68aa4 🚀

--- assets/Home-De9UqC_w.js
+++ assets/Home-De9UqC_w.js
@@ -1,4 +1,4 @@
-import{u as t,j as o}from"./index-BB8_SseR.js";const s=t.div`
+import{u as t,j as o}from"./index-CZ2R6mpc.js";const s=t.div`
   padding: 0 20px 10px;
 `,r=t.div`
   padding: 50px 20px;
