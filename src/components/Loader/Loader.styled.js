43e3589874ac6b3cdc6f08bb9b8be4e2e7a7eb07
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2024-08-03 14:26:26 +0300

    fix: import imgs for loader

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import loaderImgMobile from "../../assets/imgs/mobile/start-screen-2x-mob.jpg";
+import loaderImgTablet from "../../assets/imgs/tablet/start-screen-img-2x-tablet.jpg";
+import loaderImgDesktop from "../../assets/imgs/desctop/start-screen-img-2x-desc.jpg";
 
 export const LoaderContainer = styled.div`
   display: flex;
@@ -7,19 +10,19 @@ export const LoaderContainer = styled.div`
   width: 100%;
   height: 100vh;
   background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-    url("./src/assets/imgs/mobile/start-screen-2x-mob.jpg");
+    url(${loaderImgMobile});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
 
   @media screen and (min-width: 768px) {
     background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-      url("./src/assets/imgs/tablet/start-screen-img-2x-tablet.jpg");
+      url(${loaderImgTablet});
   }
 
   @media screen and (min-width: 1280px) {
     background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-      url("./src/assets/imgs/desctop/start-screen-img-2x-desc.jpg");
+      url(${loaderImgDesktop});
   }
 `;
 
