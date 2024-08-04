d367eea936fade4c5f27d50fb7c2f4c566e8798f
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-08-04 19:51:17 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@c56f7ceef531f83643fccc9f4505f4633a58b8de 🚀

--- assets/FindPet-Ba4gOSZx.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Cb26m6-V.js";const e=()=>t.jsx(t.Fragment,{children:"Find Your Pet"});export{e as default};
--- assets/NotFound-Bjsh8XqK.js
+++ assets/NotFound-Bjsh8XqK.js
@@ -1,4 +1,4 @@
-import{u as e,j as t,i as n}from"./index-Cb26m6-V.js";const r=e.div`
+import{u as e,j as t,i as n}from"./index-BFgCHHn9.js";const r=e.div`
   width: 335px;
   padding: 0 20px 20px;
   margin: 0 auto;
