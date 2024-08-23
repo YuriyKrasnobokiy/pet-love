14eade22a2cf3987ca6f6fe4e19450a24d764e9a
YuriyKrasnobokiy <91632730+YuriyKrasnobokiy@users.noreply.github.com>
2024-08-23 07:39:46 +0000

    Deploying to gh-pages from @ YuriyKrasnobokiy/pet-love@d857220435662244f0b15b0013ff9ed5830eb02e 🚀

--- assets/OurFriends-IwxhhviV.js
+++ assets/OurFriends-IwxhhviV.js
@@ -1,4 +1,4 @@
-import{u as t,j as i,a as p,c as o,r as h,h as x,L as m}from"./index-hF738Flj.js";const g=e=>e.friendsStore.friends,w=e=>e.friendsStore.isLoading,u=e=>e.friendsStore.error,f=t.div`
+import{u as t,j as i,a as p,c as o,r as h,h as x,L as m}from"./index-BHO3Bkw8.js";const g=e=>e.friendsStore.friends,w=e=>e.friendsStore.isLoading,u=e=>e.friendsStore.error,f=t.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
