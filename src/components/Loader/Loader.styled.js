c708f73d0e68b8264de19cd823cee4e33407152b
Yuriy Krasnobokiy <krasnobokiy.yuriy@gmail.com>
2025-05-26 22:24:25 +0300

    fix: loader styles

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -4,16 +4,20 @@ import loaderImgTablet from "../../assets/imgs/tablet/start-screen-img-2x-tablet
 import loaderImgDesktop from "../../assets/imgs/desctop/start-screen-img-2x-desc.jpg";
 
 export const LoaderContainer = styled.div`
+position: absolute;
+  top: 0;
+  left: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  height: 100dvh;
   background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
     url(${loaderImgMobile});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+  z-index: 9999;
 
   @media screen and (min-width: 768px) {
     background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
